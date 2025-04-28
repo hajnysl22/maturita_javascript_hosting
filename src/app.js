b7d9a5827459f6f792435b64962e0a88981b6bb4
Lukáš Hajnyš <hajnys.lukas@gmail.com>
2025-04-28 17:41:20 +0200

    To samé pro app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+require('dotenv').config();
+require('./db');
 const authRoutes = require('./routes/authRoutes');
 const notesRoutes = require('./routes/notesRoutes');
 
@@ -14,5 +16,7 @@ app.use(bodyParser.json());
 app.use('/auth', authRoutes);
 app.use('/notes', notesRoutes);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server běží na http://localhost:${PORT}`));
+
+
